633cea63e6f8165f20aa1d457b41f8dc52f56c0f
Adam <adamhbr91170@gmail.com>
2025-06-29 17:48:26 +0200

    correction sur le footer

--- studio-rhizom/src/layouts/DefaultLayout.vue
+++ studio-rhizom/src/layouts/DefaultLayout.vue
@@ -1,14 +1,17 @@
 <script setup>
-import Header from '../components/Header.vue'
-import Footer from '../components/Footer.vue'
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-  <div class="min-h-screen bg-white text-black font-sans">
+  <div class="flex flex-col min-h-screen bg-white text-black font-halogen">
     <Header />
-    <main class="px-4 py-8 max-w-screen-lg mx-auto">
+    <!-- Contenu principal : prend tout l'espace restant -->
+    <main class="flex-grow px-4 py-8 max-w-screen-lg mx-auto">
       <slot />
     </main>
+
+    <!-- Footer : toujours en bas -->
     <Footer />
   </div>
 </template>
