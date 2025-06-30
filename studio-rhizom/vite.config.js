0ceab6ee2ea8364a08cebe7dbd9d2b733fb3763d
Adam <adamhbr91170@gmail.com>
2025-06-30 02:03:37 +0200

    je sais plus

--- studio-rhizom/vite.config.js
+++ studio-rhizom/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  root: '.',
-  publicDir: 'public'
-})
+  root: ".",
+  publicDir: "public",
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
